711c28f21fae7239ba276c7be4e5b7c7b1606ce3
wcraddock1452 <wcraddock@murraystate.edu>
2022-04-22 13:55:09 -0500

    Update models.py

--- eyewitness/law_enforcement/models.py
+++ eyewitness/law_enforcement/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+# table for photo descriptions
 class Photo(models.Model):
     class Meta:
         verbose_name = 'Photo'
